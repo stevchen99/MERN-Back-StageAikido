8022ea7ef09288d10e7026485cbfda17d7cc8227
chens <stevchen99@gmail.com>
2025-12-10 21:32:14 +0100

    oder by date asc

--- routes/stageRoutes.js
+++ routes/stageRoutes.js
@@ -67,7 +67,7 @@ router.post('/', async (req, res) => {
  */
 router.get('/', async (req, res) => {
     try {
-        const entries = await StageEntry.find().sort({ date: -1 });
+        const entries = await StageEntry.find().sort({ date: 1 });
         res.json(entries);
     } catch (err) {
         res.status(500).json({ message: err.message });
